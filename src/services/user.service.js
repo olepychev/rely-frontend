5dc01eca5df44a6c050ec88df61480db1c5379ea
bluesky0707 <asmakech115@gmail.com>
2022-12-01 12:30:55 -0300

    for deploy

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth.header";
-const API_URL = "http://localhost:8080/api/";
-// const API_URL = "https://seashell-app-jatrt.ondigitalocean.app/api/";
+// const API_URL = "http://localhost:8080/api/";
+const API_URL = "https://seashell-app-jatrt.ondigitalocean.app/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
